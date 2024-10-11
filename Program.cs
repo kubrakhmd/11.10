607187958c9e7b9d3c0224a02bf2590cedae6de9
kubrakhmd <kubraxe-bp217@code.edu.az>
2024-10-11 10:20:00 +0400

    11.10 task added

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace StackHeapRefOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3,4,5 };
+            CustomResize(ref numbers, 15,20,25);
+
+            Console.WriteLine("New array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+        public static void CustomResize(ref int[] arr, params int[] numbers)
+        {
+            int newSize = arr.Length + numbers.Length;
+            int[] newArr = new int[newSize];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                newArr[arr.Length + i] = numbers[i];
+            }
+
+            arr = newArr;
+        }
+    }
+}
